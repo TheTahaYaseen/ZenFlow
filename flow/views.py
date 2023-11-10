074817488d21fb9f3b2980e4e311e024bd47df6a
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-10 19:43:56 +0500

    Basic Register UI Done

--- flow/views.py
+++ flow/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm 
 
 # Create your views here.
 def home_view(request):
     context = {}
     return render(request, "flow/home.html", context)
+
+def register_view(request):
+    form = UserCreationForm()
+    context = {"form": form}
+    return render(request, "flow/register.html", context)
